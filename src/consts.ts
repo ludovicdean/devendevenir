0eab605a38ac3e1d09e913056ab221bd80b67908
ludovicdean <ludovicd58@gmail.com>
2023-12-12 23:58:36 +0100

    initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Dev En Devenir';
+export const SITE_DESCRIPTION = 'Vous êtes développeur/se depuis peu ? Ce n\'était pas votre métier initial ? Ce site est fait pour vous !';
---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		date: z.coerce.date().optional(),
+		updatedDate: z.coerce.date().optional(),
+		banner: z.string().optional(),
+		heroImage: z.string().optional(),
+		photoAuthor: z.string().optional(),
+		photoAuthorLink: z.string().optional(),
+		author: z.string().optional(),
+		authorLink: z.string().optional(),
+		unsplashLink: z.string().optional(),
+		url: z.string().optional(),
+		showCreditPhoto: z.boolean().optional(),
+	}),
+});
+
+export const collections = { blog };
