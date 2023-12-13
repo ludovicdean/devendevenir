a4a10ca22c3303d13e10ede3e566825d61659155
ludovicdean <65154985+ludovicdean@users.noreply.github.com>
2023-12-13 14:22:10 +0100

    Update astro.config.mjs
    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ludovic.dean.github.io.com',
+	base: '/devendevenir',
 	integrations: [mdx(), sitemap()],
 	output: "static"
 });
