99782b2b136ae538e74a3b73c32bf92bf844b7d6
ludovicdean <ludovicd58@gmail.com>
2023-12-13 13:36:02 +0100

    delete base param

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ludovic.dean.github.io.com',
-	base: '/blog',
 	integrations: [mdx(), sitemap()],
 	output: "static"
 });
