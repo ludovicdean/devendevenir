508767ec225a11e93ec3318c9e45fb0787a39394
ludovicdean <ludovicd58@gmail.com>
2023-12-13 12:53:35 +0100

    add deployment file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://ludovic.dean.github.io.com',
+	base: '/blog',
 	integrations: [mdx(), sitemap()],
 });
