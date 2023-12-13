6200d869b2f78ba84a243df60a23e7705f745fd5
ludovicdean <ludovicd58@gmail.com>
2023-12-13 22:58:06 +0100

    modify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ludovic.dean.github.io.com',
+	site: 'https://ludovic.dean.github.io',
+	base: '/astro-blog',
 	integrations: [mdx(), sitemap()],
 	output: "static"
 });
