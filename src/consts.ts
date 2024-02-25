3d7a25f0a95a753efeaba2e0312bd617528be4b5
ludovicdean <ludovicd58@gmail.com>
2024-02-25 15:15:04 +0100

    add links to consts

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,5 @@
 
 export const SITE_TITLE = 'Dev En Devenir';
 export const SITE_DESCRIPTION = 'Vous êtes développeur/se depuis peu ? Ce n\'était pas votre métier initial ? Ce site est fait pour vous !';
+export const LINKEDIN_LINK = "https://www.linkedin.com/in/ludovic-dean/";
+export const GITHUB_LINK = "https://github.com/ludovicdean/astro-blog";
