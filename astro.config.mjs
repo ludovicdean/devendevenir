660358019cafb4db83eb2460b5099193494e5fec
ludovicdean <ludovicd58@gmail.com>
2024-10-22 23:38:26 +0200

    (BLG-46) ajout netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon"
+import icon from "astro-icon";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ludovic.dean.github.io',
   base: '/devendevenir',
   integrations: [mdx(), sitemap(), tailwind(), icon()],
-  output: "static"
+  output: "static",
+  adapter: netlify()
 });
