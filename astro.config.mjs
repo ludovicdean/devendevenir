ef5c1b7da9ee28c46db3f7fda4fee7b9b43fbb77
ludovicdean <ludovicd58@gmail.com>
2025-03-08 14:10:17 +0100

    test pour bloghelper

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,19 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon"
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ludovic.dean.github.io',
   base: '/devendevenir',
-  integrations: [mdx(), sitemap(), tailwind(), icon()],
+  middleware: ['./src/middleware.js'],
+  integrations: [
+    mdx(), 
+    sitemap(), 
+    tailwind(), 
+    icon(),
+    
+  ],
   output: "static"
 });
