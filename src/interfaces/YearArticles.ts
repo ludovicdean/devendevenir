c65679dfe20d8b1709c1f844f790e0d1ae2e19a2
ludo <ludovicd58@gmail.com>
2025-04-01 22:37:48 +0200

    (BLG-69) publication article hash, suppression google tag manager et révision CallToAction

---
+++ src/interfaces/YearArticles.ts
@@ -0,0 +1,4 @@
+export interface YearArticles {
+    year: number;
+    articles: any[];
+    }
---
+++ src/pages/api/unpublished.json.ts
@@ -0,0 +1,14 @@
+import { getCollection } from 'astro:content';
+
+export async function GET() {
+  const blogEntries = await getCollection('blog');
+  // const frontmatterData = blogEntries.map(entry => entry.data);
+  const frontmatterData = blogEntries.sort((a, b) => b.data.date?.valueOf() - a.data.date?.valueOf())
+  .map(entry => ({
+    id: entry.id,
+    data: entry.data,
+isMax: entry.body == undefined ?true: false
+  })).filter(entry => entry.data.date === undefined);
+  console.log(frontmatterData);
+  return new Response(JSON.stringify(frontmatterData));
+}
