a6854ba4dcd01ac5083cca16b2ae10df4cb55611
ludo <ludovicd58@gmail.com>
2025-07-05 13:59:20 +0200

    inversion page test et index

--- src/pages/api/unpublished.json.ts
+++ src/pages/api/unpublished.json.ts
@@ -1,13 +1,14 @@
-import { getCollection } from 'astro:content';
+import { getUnpublishedPosts } from 'src/utils/blogUtils';
 
 export async function GET() {
-  const blogEntries = await getCollection('blog');
-  // const frontmatterData = blogEntries.map(entry => entry.data);
-  const frontmatterData = blogEntries.sort((a, b) => b.data.date?.valueOf() - a.data.date?.valueOf())
+  const blogEntries = await getUnpublishedPosts();
+
+  const frontmatterData = blogEntries
   .map(entry => ({
     id: entry.id,
     data: entry.data,
-isMax: entry.body == undefined ?true: false
-  })).filter(entry => entry.data.date === undefined);
+    isMax: entry.body == undefined ?true: false
+  }));
+
   return new Response(JSON.stringify(frontmatterData));
 }
