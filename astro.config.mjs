ae26087cbce915f1858006c4beaec41ce1bc996a
ludo <ludovicd58@gmail.com>
2025-07-05 13:41:25 +0200

    ajout expressive code et nettoyage

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,19 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import expressiveCode from 'astro-expressive-code';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ludovic.dean.github.io',
   base: '/devendevenir',
   middleware: ['./src/middleware.js'],
   integrations: [
-    mdx(), 
-    sitemap(), 
-    tailwind(), 
+    expressiveCode(),
+    mdx(),
+    sitemap(),
+    tailwind(),
     icon(),
-    
   ],
   output: "static"
 });
