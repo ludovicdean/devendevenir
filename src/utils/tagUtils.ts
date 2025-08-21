526ab94731dcb8865b06090f5b22c1d8298d00d1
ludo <ludovicd58@gmail.com>
2025-08-21 23:00:47 +0200

    feat(BLG-72)  gestion des tags par id et renommage des layouts

--- src/utils/tagUtils.ts
+++ src/utils/tagUtils.ts
@@ -1,14 +1,31 @@
+import { getCollection } from "astro:content";
 import { getPosts } from "./blogUtils";
 
-export async function getTags(){
+export async function getTagsCount() {
     const posts = await getPosts();
-    
-    const allTags = posts.map((post) => post.data.tags).flat();
-    
-    const tagCounts = allTags.reduce((acc, tag: string) => {
-      acc[tag] = (acc[tag] || 0) + 1;
-      return acc;
-    }, {} as Record<string, number>);
+    const tagsCollection = await getCollection('tags');
 
-    return tagCounts;
+    // Utilise les ids des tags pour le comptage
+    const allTagIds = posts
+        .map(post => post.data.tags)
+        .flat()
+        .filter(Boolean);
+
+    const tagCounts = allTagIds.reduce((acc, tagId) => {
+        acc[tagId] = (acc[tagId] || 0) + 1;
+        return acc;
+    }, {});
+
+    const tagsWithCounts = tagsCollection.map(tag => {
+        const id = tag.id;
+        const name = tag.data.name || id;
+        const count = tagCounts[id] || 0;
+        return { id, name, count };
+    });
+
+    return tagsWithCounts;
+}
+
+export async function getTags() {
+    return await getCollection('tags');
 }
