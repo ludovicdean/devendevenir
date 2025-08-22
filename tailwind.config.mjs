c827e5ef3eb8de1ba8cd09e028b0001474548b57
ludo <ludovicd58@gmail.com>
2025-08-22 22:46:45 +0200

    feat (BLG-72) suppression config tailwind inutile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,17 +4,7 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				'accent': "var(--accent)",
-				'accent-dark': "var(--accent-dark)",
-				'black': "var(--black)",
 				'gray': "var(--gray)",
-				'gray-dark': "var(--gray-dark)",
-				'gray-light': "var(--gray-light)",
-				'gray-gradient': "var(--gray-gradient)",
-				'box-shadow': "var(--box-shadow)"
-			},
-			backgroundColor: {
-				'custom-gray': "var(--gray)",
 			}
 		},
 	},
