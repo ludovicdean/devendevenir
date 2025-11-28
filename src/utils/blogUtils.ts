915f15c4b08d286e8f236f18b58a3decda935544
ludovicdean <ludovicd58@gmail.com>
2025-11-29 00:27:08 +0100

    feat(BLG-72) refactorisation articles similaires et layout des tags

--- src/utils/blogUtils.ts
+++ src/utils/blogUtils.ts
@@ -1,28 +1,40 @@
 import { getCollection } from "astro:content";
 const base = import.meta.env.BASE_URL;
 
-export async function getPosts(){
-    const posts = (await getCollection('blog')).filter(post => post.id !== undefined && !post.id.startsWith('_')).sort((a, b) =>
-        {
-            const dateA = a.data?.date instanceof Date ? a.data.date.valueOf() : 0;
-            const dateB = b.data?.date instanceof Date ? b.data.date.valueOf() : 0;
-            return dateB - dateA;
-        });
-return posts;
-}
-
-export async function getUnpublishedPosts(){
-    const posts = (await getCollection('blog')).filter(post => post.id !== undefined && post.id.startsWith('_')).sort((a, b) =>
-        {
-            const dateA = a.data?.date instanceof Date ? a.data.date.valueOf() : 0;
-            const dateB = b.data?.date instanceof Date ? b.data.date.valueOf() : 0;
-            return dateB - dateA;
-        });
-    
+export async function getPosts() {
+    const posts = (await getCollection('blog')).filter(post => post.id !== undefined && !post.id.startsWith('_')).sort((a, b) => {
+        const dateA = a.data?.date instanceof Date ? a.data.date.valueOf() : 0;
+        const dateB = b.data?.date instanceof Date ? b.data.date.valueOf() : 0;
+        return dateB - dateA;
+    });
     return posts;
 }
 
-export function getUrl(post){
+export async function getUnpublishedPosts() {
+    const posts = (await getCollection('blog')).filter(post => post.id !== undefined && post.id.startsWith('_')).sort((a, b) => {
+        const dateA = a.data?.date instanceof Date ? a.data.date.valueOf() : 0;
+        const dateB = b.data?.date instanceof Date ? b.data.date.valueOf() : 0;
+        return dateB - dateA;
+    });
+
+    return posts;
+}
+
+export function getUrl(post) {
     return post.data.url ?? `${base + "/blog/" + post.id}/`
 }
 
+export async function getBlogsByTagId(tagId: string) {
+    const posts = await getPosts();
+    return posts.filter((post) => !post.id.startsWith('_') && post.data.tags?.includes(tagId));
+}
+
+export async function getSimilarPosts(tags: string[], currentTitle: string) {
+    const posts = await getPosts();
+
+    const similarPosts = posts.filter(
+        (e) => tags.some((t) => e.data.tags?.includes(t)) && e.data.title !== currentTitle,
+    ).slice(0, 3);
+
+    return similarPosts;
+}
