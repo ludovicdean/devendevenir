f38be8028a8e64d264eefdee72dd57a52e221d7d
ludovicdean <ludovicd58@gmail.com>
2025-11-29 00:12:33 +0100

    feat(BLG-72) modification api tags

--- src/pages/api/tags.json.ts
+++ src/pages/api/tags.json.ts
@@ -1,22 +1,7 @@
-import { getCollection } from 'astro:content';
-import type { Tag } from 'src/interfaces/Tag';
 import { getTagsCount } from 'src/utils/tagUtils';
 
 export async function GET() {
-  
-  const tagCounts = await getTagsCount();
+  const tagsWithCounts = await getTagsCount();
 
-  const fullTags = await getCollection("tags");
-
-  const tags: Tag[] = Object.entries(tagCounts).map(([name, count]) => {
-    const tagInfo = fullTags.find(t => t.data.name === name);
-    return {
-      id: tagInfo?.id,
-      name,
-      description: tagInfo ? tagInfo.data.description : "",
-      nombreArticles: count
-    };
-  });
-
-  return new Response(JSON.stringify(tags));
+  return new Response(JSON.stringify(tagsWithCounts));
 }
