d114a7a6b788fc7e7d220448a1776e50eb5bb725
ludovicdean <ludovicd58@gmail.com>
2025-11-29 23:20:59 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,13 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
 import expressiveCode from 'astro-expressive-code';
-
+console.log('ASTRO_BASE in config:', process.env.ASTRO_BASE);
 const base = process.env.ASTRO_BASE || '/devendevenir';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ludovicdean.github.io',
-  base,
+  base: process.env.ASTRO_BASE || '/devendevenir',
   middleware: ['./src/middleware.js'],
   integrations: [
     expressiveCode(),
