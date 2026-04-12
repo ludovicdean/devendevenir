40f684386f91fe514c138d9c36b256bb387a71af
ludovicdean <ludovicd58@gmail.com>
2026-04-12 23:36:31 +0200

    gestion tailwind vs css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,12 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {
-			colors: {
-				'gray': "var(--gray)",
-			}
-		},
+		extend: {},
 	},
+
 	plugins: [require('@tailwindcss/aspect-ratio')]
 ,
 }
